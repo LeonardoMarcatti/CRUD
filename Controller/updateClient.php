1d490d1c5a50a35b721db891a3c3092339472b3e
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2022-01-10 12:08:12 -0300

    Fixed some small bugs

--- Controller/updateClient.php
+++ Controller/updateClient.php
@@ -158,33 +158,14 @@
         $new_cliente_telefone->setIDCliente($client_id);
         $new_clientetelefone_dao->update($new_cliente_telefone, $current_tel_id);
         
-        if (!empty($email)) {
+        if ($email) {
             $new_email->setAddress($email);
-<<<<<<< HEAD
-            $checked_email = $new_email_dao->checkEmail($new_email);
-            if (!empty($checked_email)) {
-                if ($checked_email->getClienteID() != $client_id) {
-                    $_SESSION['flash'] = 'Email em uso'; 
-                } else {
-                    $new_email->setClienteID($client_id);
-                    $new_email_dao->update($new_email);
-                };
-            } elseif($client_id && ($current_email_id != '')){
-                $new_email->setClienteID($client_id);
-                $new_email_dao->update($new_email);
-                echo 'a';
-            } else{
-                $new_email->setClienteID($client_id);
-                $new_email_dao->add($new_email);
-                echo 'b';
-=======
             if ($new_email_dao->checkEmail($new_email)) {
                 return false;
             } else {
                 $new_email->setClienteID($client_id);
                 $new_email->setAddress($email);
                 $new_email_dao->update($new_email);
->>>>>>> master
             };
         }else{
             $new_email_dao->delete($client_id);
